f2baff25d14f369f4c39b9b31de1c12a64ce18b0
Zephyr709 <jdcooper.dev@gmail.com>
2023-01-28 15:04:13 -0330

    .105

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # test-site-one
 place to test different things during the learning process
+
+Changelog
+.104 js file reset
+.105 {
+    JS- added the ability to change image on click
+        created a function to prompt user for a name and display that name on the webpage as well as store it locally in the browser
+        linked HTML button to the setUserName function
+
+    HTML- Added a button to the webpage to interact with the setuserName function
+          Changed the title, h1, and h2 text content
+
+    CSS- changed the color of h1
+         aligned position of h2
+    }
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,39 @@
 //Javascript comment
+
+/**
+ * Another JavaScript comment
+ */
+
+const myImage = document.querySelector('img');
+
+myImage.onclick = () => {
+    const mySrc =myImage.getAttribute("src");
+    if (mySrc ==="images/black-hole-1.jpg") {
+        myImage.setAttribute("src", "images/black-hole-2.jpg")
+    }
+    else {
+        myImage.setAttribute("src", "images/black-hole-1.jpg")
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading2 = document.querySelector("h2");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading2.textContent = `Welcome back, ${myName}`;
+}
+
+/*if(!localStorage.getItem("name")) {
+    setUserName();
+} else{
+    const storedName = localStorage.getItem("name");
+    if(storedName !== null){
+        myHeading2.textContent = `Welcome back, ${storedName}`;
+    }
+}*/
+
+myButton.onclick = () => {
+    setUserName();
+}
